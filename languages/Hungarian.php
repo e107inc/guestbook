1528859f535f87fdd6d84b21859545decaf59ebf
Gábor <yesszus@gmail.com>
2016-12-14 09:50:28 +0100

    Create Hungarian.php

---
+++ languages/Hungarian.php
@@ -0,0 +1,115 @@
+<?php
+/*
+  ---------------------------------------------------------------------------------------+
+  |
+  |	e107 website system
+  |	GUESTBOOK PLUGIN V4.0
+  |
+  |	Released under the terms and conditions of the
+  |	GNU General Public License Version 2 (http://gnu.org).
+  |
+  ---------------------------------------------------------------------------------------+
+  | original: ©Andrew Rockwell 2003
+  |	      http://2sdw.com
+  |           chavo@2sdw.com
+  +--------------------------------------------------------------------------------------+
+  | updates:  ©Richard Perry 2005
+  |           http://www.greycube.com
+  |           code@greycube.com
+  +--------------------------------------------------------------------------------------+
+  | updates:  ©Titanik 2007
+  |          http://upc.utc.sk
+  |           tomasss@inmail.sk
+  +--------------------------------------------------------------------------------------+
+  | updates:  ©Smarti October 2007
+  |          http://www.platinwebservice.de
+  |           webmaster@platinwebservice.de
+  +--------------------------------------------------------------------------------------+
+  
+  +---------------------------------------------------------------+
+  |        e107 website content management system Hungarian Language File
+  |        Released under the terms and conditions of the
+  |        GNU General Public License (http://gnu.org).
+  |        Last Modified: 2016/02/22 13:15:43
+  |
+  |        $Author: Yesszus $
+  +---------------------------------------------------------------+
+*/
+
+define("GB_LAN_SIGN","Írás a Vendégkönyvbe");
+define("GB_LAN_NOTICE","- HTML nem engedélyezett!<br />- Email cím nem publikus.<br />- 10 perc áll a rendelkezésére a bejegyzés módosítására.");
+define("GB_LAN_NAME","Név");
+define("GB_LAN_EMAIL","Email");
+define("GB_LAN_WEBSITE","Weboldal");
+define("GB_LAN_COMMENT","Hozzászólás");
+define("GB_LAN_EMOTES","Hangulatjelek");
+define("GB_LAN_SUBMIT","Mehet");
+define("GB_LAN_SECURE", "Adja meg a képen látható szöveget");
+define("GB_LAN_WRONGCODE","Ön rossz számot adott meg");
+define("GB_LAN_UPDATED","A bejegyzés frissítve!");
+define("GB_LAN_THANKYOU","Köszönjük, hogy írt a vendégkönyvbe!");
+define("GB_LAN_THANKYOUMOD", "Köszönjük, hogy írt a vendégkönyvbe!<br />Egy Admin hamarosan aktiválni fogja.");
+define("GB_LAN_REPEAT","Ön már írt a vendégkönyvbe!");
+define("GB_LAN_CONFIRM","Erősítse meg");
+define("GB_LAN_PERMISSION","Ön nem jogosult erre");
+define("GB_LAN_NOHTML","A HTML nem engedélyezett!");
+define("GB_LAN_NOLINKS","A hozzászólásban nem engedélyezettek a linkek");
+define("GB_LAN_GOBACK","Vissza");
+
+define("GB_LAN_EDIT","Szerkeszt");
+define("GB_LAN_DELETE","Töröl");
+define("GB_LAN_PROFILE","Profil");
+
+define("GB_LAN_DONE", "A Plugin sikeresen telepítve lett! - Most lépjen a beállításokhoz.");
+define("GB_LAN_UPDONE", "Sikeres frissítés!");
+define("GB_LAN_ADM_NAME", "Vendégkönyv");
+define("GB_LAN_ADM_1", "Bejegyzés");
+define("GB_LAN_ADM_2", "Név");
+define("GB_LAN_ADM_3", "Állapot");
+define("GB_LAN_ADM_4", "Opció");
+define("GB_LAN_ADM_5", "Aktiválás");
+define("GB_LAN_ADM_6", "Törlés");
+define("GB_LAN_ADM_7", "Bejegyzés dátuma");
+define("GB_LAN_ADM_8", "IP/HOSZT ");
+define("GB_LAN_ADM_9", "Blokkolás");
+define("GB_LAN_ADM_10",	"Bejegyzések száma per oldal");
+define("GB_LAN_ADM_11",	"Vendégkönyv Címe");
+define("GB_LAN_ADM_12",	"Oldal stilus hozzárendelése");
+define("GB_LAN_ADM_13",	"A BBCode és eszköztár engedélyezése");
+define("GB_LAN_ADM_14",	"ismétlődő bejegyzések blokkolása");
+define("GB_LAN_ADM_15",	"A linkek nem engedélyezettek a bejegyzésekben");
+define("GB_LAN_ADM_16",	"Csak a felhasználók láthatják a weboldalakat");
+define("GB_LAN_ADM_17",	"Opciók frissítése ");
+define("GB_LAN_ADM_18",	"Az opciók frissítve");
+define("GB_LAN_ADM_19", "Biztonsági Kép Kód Ellenőrzés engedélyezése");
+define("GB_LAN_ADM_20",	"Vendégkönyv bejegyzések");
+define("GB_LAN_ADM_21",	"Új vendégkönyv bejegyzések");
+define("GB_LAN_ADM_22",	"Az új bejegyzések csak az admin ellenőrzés után jelennek meg");
+define("GB_LAN_ADM_23", "Új bejegyzés");
+define("GB_LAN_ADM_24", "Aktiválva");
+define("GB_LAN_ADM_25",	"Bejegyzés azonosító ");
+define("GB_LAN_ADM_26",	" aktiválva.");
+define("GB_LAN_ADM_27",	" törölve.");
+define("GB_LAN_ADM_28", "Nincs új vendégkönyv bejegyzés.");
+define("GB_LAN_ADM_29", "Moderátor Csoport");
+define("GB_LAN_ADM_30", "Szerkesztési idő (Perc)");
+define("GB_LAN_ADM_31", " blokkolva.");
+define("GB_LAN_ADM_32", "A bejegyzés blokkolva!");
+
+
+define("GB_LAN_M_1", "Opciók");
+define("GB_LAN_M_2", "Beállítások");
+define("GB_LAN_M_3", "Bejegyzések megtekintése");
+define("GB_LAN_M_4", "Új bejegyzések");
+define("GB_LAN_M_5", "Blokkolt bejegyzések");
+
+
+define("GB_LAN_NT_1", "Vendégköny események");
+define("GB_LAN_NT_2", "Új vendégkönyv bejegyzés lett beküldve.");
+define("GB_LAN_NT_3", "Beküldő");
+define("GB_LAN_NT_4", "IP cím");
+define("GB_LAN_NT_5", "Bejegyzés szövege");
+define("GB_LAN_NT_6", "Vendégkönyv üzenet írásakor");
+define("GB_LAN_NT_7", "Figyelmeztetés: <span style='color:red'>Az Email értesítés letiltva! <a href ='".e_ADMIN."notify.php'>ITT engedélyezheti</a></span>");
+define("GB_LAN_NT_8", "Figyelmeztetés: <span style='color:green'>Az Email értesítés engedélyezve az új bejegyzések beküldésekor.</span>");
+?>
